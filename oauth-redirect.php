25954c322a4dc1788b85a872de0a9b7e4207f23f
Chirag Bavaria <chirag.b@vtechsolution.us>
2018-06-11 22:29:29 +0530

    Added Index with curl

--- oauth-redirect.php
+++ oauth-redirect.php
@@ -9,4 +9,10 @@ if (empty($session_id))
   $_SESSION['code'] = $_GET['code'];
   $_SESSION['realmId'] = $_GET['realmId'];
 
+  $myfile = fopen("token.txt", "w") or die("Unable to open file!");
+  $txt = "John Doe\n";
+  fwrite($myfile, $txt);
+  $txt = "Jane Doe\n";
+  fwrite($myfile, $txt);
+  fclose($myfile);
 ?>
